055de06f1b2c812ee544588c5b9a69f901223b9a
EfremBAK <efrem.berhane2009@gmail.com>
2019-12-03 16:55:49 -0500

    third commit

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,8 +1,13 @@
 package com.example.demo;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+
 @Controller
 public class HomeController {
     @RequestMapping("/")
@@ -19,4 +24,13 @@ public class HomeController {
     public String admin(){
         return "admin";
     }
+
+    @Autowired
+    UserRepository userRepository;
+    public String secure(Principal principal, Model model){
+        String username = principal.getName();
+        model.addAttribute("user", userRepository.findByUsername(username));
+        return "secure";
+    }
+
 }
